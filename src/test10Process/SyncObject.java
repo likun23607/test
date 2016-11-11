6d41f935362a246ebea057321e0487387099e536
likun23607 <609327216@qq.com>
2016-11-11 23:30:00 +0800

    adddddddd

--- src/test10Process/SyncObject.java
+++ src/test10Process/SyncObject.java
@@ -14,4 +14,25 @@ public class SyncObject {
         }.start();
         ds.g();
     }
+
+    /**
+     * Created by Administrator on 2016/11/11.
+     */
+    public static class DualSynch {
+        private Object syncObject=new Object();
+        public synchronized void f()   {
+            for (int i=0;i<5;i++) {
+                System.out.println("f()" + Thread.currentThread());
+                Thread.yield();
+            }
+        }
+        public void g()   {
+            synchronized (syncObject){
+                for (int i=0;i<5;i++){
+                    System.out.println("g()"+Thread.currentThread());
+                    Thread.yield();
+                }
+            }
+        }
+    }
 }
