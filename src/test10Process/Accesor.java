fe34bebe1b52d12053a554e400bcddb4b7196101
likun23607 <609327216@qq.com>
2016-11-11 23:25:14 +0800

    adddddddd

--- src/test10Process/Accesor.java
+++ src/test10Process/Accesor.java
@@ -4,8 +4,12 @@ package test10Process;
  * Created by Administrator on 2016/11/11.
  */
 public class Accesor implements Runnable{
+    private final int id;
+    public Accesor(int idn){
+        id=idn;
+    }
     @Override
     public void run() {
-            
+
     }
 }
